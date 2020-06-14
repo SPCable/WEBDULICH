7d533f8d059682813913ec17ddf7b95b43dc7781
SPCable <44009901+SPCable@users.noreply.github.com>
2020-06-14 17:00:47 +0700

    iuiuiu

---
+++ web/Controllers/NguoiDungController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: NguoiDung
+        public ActionResult DangKi()    
+        {
+            return View();
+        }
+
+
+    }
+}
